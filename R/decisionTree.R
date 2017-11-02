e4ae5a28525ac653161efa14db88778c60e40f3c
tomek <tomis9@buziaczek.pl>
2017-11-02 11:49:19 +0100

    only aesthetics of code

--- R/decisionTree.R
+++ R/decisionTree.R
@@ -1,7 +1,7 @@
 #' decisionTree
 #'
 #' Decision tree algorithm uses information gain (entropy), works both for categorical and numerical attributes. Based on Zaki, Meira Jr., Data Mining and Analysis, p.481-496.
-#' @param d data frame, dependant variable must be in the first column
+#' @param d data.frame, dependant variable must be in the first column
 #' @param eta stop criterium, size of a leaf
 #' @param purity stop criterium, purity of a leaf
 #' @param minsplit do not split nodes that are smaller than minsplit
@@ -17,217 +17,216 @@
 #' x[d$Sepal.Length >  6.1 & d$Sepal.Length <= 7.0] <- "Long"
 #' x[d$Sepal.Length >  7.0] <- "Very Long"
 #' d$Sepal.Length <- x
-#' decisionTree(d, eta=5, purity=0.95, minsplit=0)
+#' decisionTree(d, eta = 5, purity=0.95, minsplit=0)
 
 # ---- decisionTree
-decisionTree <- function(d, eta=10, purity=0.95, minsplit=10) {
-    resultDF <- data.frame(level=numeric(), 
-                           parent=numeric(),
-                           node=numeric(), 
-                           leaf=numeric(),
-                           RL=character(),
-                           Lsize=numeric(), 
-                           Lleft=numeric(), 
-                           Lright=numeric(), 
-                           Rsize=numeric(), 
-                           Rleft=numeric(), 
-                           Rright=numeric(), 
-                           Rsize=numeric(),
-                           v=character(),
-                           vC=character(),  # complement to v
-                           vName=character(),
-                           stringsAsFactors=F)
-    nodeChoices <- list()
-    cs <- list()  # cluster uniques
-    for (coln in colnames(d)) {
-        if (is.character(d[[coln]])) {
-            cs[[coln]] <- unique(d[[coln]])
-        }
+decisionTree <- function(d, eta = 10, purity = 0.95, minsplit = 10) {
+  resultDF <- data.frame(level = numeric(), 
+                         parent = numeric(),
+                         node = numeric(), 
+                         leaf = numeric(),
+                         RL = character(),
+                         Lsize = numeric(), 
+                         Lleft = numeric(), 
+                         Lright = numeric(), 
+                         Rsize = numeric(), 
+                         Rleft = numeric(), 
+                         Rright = numeric(), 
+                         Rsize = numeric(),
+                         v = character(),
+                         vC = character(),  # complement to v
+                         vName = character(),
+                         stringsAsFactors = F)
+  nodeChoices <- list()
+  cs <- list()  # cluster uniques
+  for (coln in colnames(d)) {
+    if (is.character(d[[coln]])) {
+      cs[[coln]] <- unique(d[[coln]])
     }
-    node <- 0
-    leaf <- 0
-    baseEnv <- environment()
-    .decisionTreeRecursive(d, eta=eta, purity=purity, LR="root", 
-                           env=baseEnv, allX=unique(d[[1]]), 
-                           minsplit=minsplit, cs=cs)
-    resultDF$level <- resultDF$level - min(resultDF$level) + 1  # scaling to 1
-    dto <- new("DecisionTreeObject", resultDF=resultDF, nodeChoices=nodeChoices)
-    return(dto)
+  }
+  node <- 0
+  leaf <- 0
+  baseEnv <- environment()
+  .decisionTreeRecursive(d, eta = eta, purity=purity, LR="root", 
+                         env = baseEnv, allX=unique(d[[1]]), 
+                         minsplit = minsplit, cs=cs)
+  resultDF$level <- resultDF$level - min(resultDF$level) + 1  # scaling to 1
+  dto <- new("DecisionTreeObject", 
+             resultDF = resultDF, 
+             nodeChoices=nodeChoices)
+  return(dto)
 }
 
 # ---- DecisionTreeObject
 DecisionTreeObject <- setClass("DecisionTreeObject", 
-                               representation(resultDF="data.frame", 
-                                              nodeChoices="list"))
+                               representation(resultDF = "data.frame", 
+                                              nodeChoices = "list"))
 
 # ---- .evaluatNumericAttribute
 .evaluateNumericAttribute <- function(d, x, minsplit) {
-    # should be a private function
-    cnames <- unique(d[[1]])
-    d <- d[order(d[[x]]),]  # sort D on attribute X
-    n <- nrow(d)
-    k <- length(unique(d[[1]]))
-    vs <- rep(0, n-1)
-    for (i in 1:(n-1)) vs[i] <- (d[[x]][i] + d[[x]][i+1]) / 2  # midpoints
-    vs <- unique(vs)
+  cnames <- unique(d[[1]])
+  d <- d[order(d[[x]]),]  # sort D on attribute X
+  n <- nrow(d)
+  k <- length(unique(d[[1]]))
 
-    best_H <- Inf
-    best_v <- 0
-    for (v in vs) {
-        dy <- d[d[[x]] <  v,]
-        dn <- d[d[[x]] >= v,]
-        if (nrow(dy) < minsplit | nrow(dn) < minsplit) next()
-        nv1 <- table(dy[[1]])
-        nv2 <- table(dn[[1]])
-        pcdy <- .entropy(nv1, cnames)
-        pcdn <- .entropy(nv2, cnames)
-        H1 <- sum(nv1) / n * pcdy + sum(nv2) / n * pcdn
-        if (H1 < best_H) {
-            best_H <- H1
-            best_v <- v
-        }
+  # midpoints
+  vs <- rep(0, n-1)
+  for (i in 1:(n-1)) vs[i] <- (d[[x]][i] + d[[x]][i+1]) / 2
+  vs <- unique(vs)
+
+  best_H <- Inf
+  best_v <- 0
+  for (v in vs) {
+    dy <- d[d[[x]] <  v,]
+    dn <- d[d[[x]] >= v,]
+    if (nrow(dy) < minsplit | nrow(dn) < minsplit) next()
+    nv1 <- table(dy[[1]])
+    nv2 <- table(dn[[1]])
+    pcdy <- .entropy(nv1, cnames)
+    pcdn <- .entropy(nv2, cnames)
+    H1 <- sum(nv1) / n * pcdy + sum(nv2) / n * pcdn
+    if (H1 < best_H) {
+      best_H <- H1
+      best_v <- v
     }
-    n0 <- table(d[[1]])
-    H0 <- -sum(n0/n * log(n0/n, 2))
-    result <- list(v=best_v, score=H0 - best_H, X=x)
-    return(result)
+  }
+  n0 <- table(d[[1]])
+  H0 <- -sum(n0/n * log(n0/n, 2))
+  result <- list(v = best_v, score=H0 - best_H, X=x)
+  return(result)
 }
 
 # ---- .evaluateCategoricalAttribute
 .evaluateCategoricalAttribute <- function(d, x, minsplit) {
-    # should be a private function
-    cnames <- unique(d[[1]])
-    n <- nrow(d)
-    k <- length(cnames)
-    V <- unique(d[[x]])
-    m <- length(V)
-    vs <- list()
-    for (i in 1:floor(m/2)) {
-        vs <- c(vs, combn(V, i, simplify=F))     
-    }
+  # should be a private function
+  cnames <- unique(d[[1]])
+  n <- nrow(d)
+  k <- length(cnames)
+  V <- unique(d[[x]])
+  m <- length(V)
+  vs <- list()
+  for (i in 1:floor(m / 2)) {
+    vs <- c(vs, combn(V, i, simplify = F))     
+  }
 
-    best_H <- Inf
-    best_v <- 0
-    for (v in vs) {
-        dy <- d[d[[x]] %in%  v,]
-        dn <- d[!d[[x]] %in% v,]
-        if (nrow(dy) < minsplit | nrow(dn) < minsplit) next()
-        nv1 <- table(dy[[1]])
-        nv2 <- table(dn[[1]])
-        pcdy <- .entropy(nv1, cnames)
-        pcdn <- .entropy(nv2, cnames)
-        H1 <- sum(nv1) / n * pcdy + sum(nv2) / n * pcdn
-        if (H1 < best_H) {
-            best_H <- H1
-            best_v <- v
-        }
+  best_H <- Inf
+  best_v <- 0
+  for (v in vs) {
+    dy <- d[d[[x]] %in%  v,]
+    dn <- d[!d[[x]] %in% v,]
+    if (nrow(dy) < minsplit | nrow(dn) < minsplit) next()
+    nv1 <- table(dy[[1]])
+    nv2 <- table(dn[[1]])
+    pcdy <- .entropy(nv1, cnames)
+    pcdn <- .entropy(nv2, cnames)
+    H1 <- sum(nv1) / n * pcdy + sum(nv2) / n * pcdn
+    if (H1 < best_H) {
+      best_H <- H1
+      best_v <- v
     }
-    n0 <- table(d[[1]])
-    H0 <- -sum(n0/n * log(n0/n, 2))
-    result <- list(v=best_v, score=H0 - best_H, X=x)
-    return(result)
+  }
+  n0 <- table(d[[1]])
+  H0 <- -sum(n0 / n * log(n0 / n, 2))
+  result <- list(v = best_v, score=H0 - best_H, X=x)
+  return(result)
 }
 
 # ---- .entropy
 .entropy <- function(nv, cnames) {
-    e <- 0  # entropy
-    n <- sum(nv)
-    for (cname in cnames)
-        if (!is.na(nv[cname])) 
-            e <- e - (nv[cname] / n) * log(nv[cname] / n, 2)
-    return(e)
+  e <- 0  # entropy
+  n <- sum(nv)
+  for (cname in cnames)
+    if (!is.na(nv[cname])) 
+      e <- e - (nv[cname] / n) * log(nv[cname] / n, 2)
+  return(e)
 }
 
 # ---- .decisionTreeRecursive
-.decisionTreeRecursive <- function(d, eta, purity, LR, env, allX, minsplit=10, cs) {
-    d_purity <- max(table(d[[1]]) / nrow(d))
-    level <- length(sys.frames())
-    parent <- 0
-    for (i in nrow(env$resultDF):1) {  # get parent node number
-        if (!nrow(env$resultDF)) {
-            break
-        } else if (env$resultDF$level[i] == level - 1) {
-            parent <- env$resultDF$node[i]
-            break
-        }
+.decisionTreeRecursive <- function(d, eta, purity, LR, env, allX, 
+                                   minsplit = 10, cs) {
+  d_purity <- max(table(d[[1]]) / nrow(d))
+  level <- length(sys.frames())
+  parent <- 0
+  for (i in nrow(env$resultDF):1) {  # get parent node number
+    if (!nrow(env$resultDF)) {
+      break
+    } else if (env$resultDF$level[i] == level - 1) {
+      parent <- env$resultDF$node[i]
+      break
     }
-    if (nrow(d) < eta | d_purity > purity) {  # leaf
-        env$node <- env$node + 1
-        env$leaf <- env$leaf + 1
-        te <- data.frame(level=level,
-                         parent=parent,
-                         node=env$node,
-                         leaf=env$leaf,
-                         LR=LR,
-                         Lsize=nrow(d),
-                         Lleft=if(is.na(table(d[[1]])[allX][1])) 0 else table(d[[1]])[allX][1],
-                         Lright=if(is.na(table(d[[1]])[allX][2])) 0 else table(d[[1]])[allX][2],
-                         Rsize=0,
-                         Rleft=0,
-                         Rright=0,
-                         v="",
-                         vC="",
-                         vName="leaf",
-                         stringsAsFactors=F)
-        rownames(te) <- NULL
-        env$resultDF <- rbind(env$resultDF, te)
-        return() 
-    } else {  # node
-        nodeChoices <- data.frame(res0=character(), score=character(), 
-                                  v=character(), stringsAsFactors=F)
-        dd <- ncol(d) - 1  # number of attributes
-        result <- list(v=0, score=0, X="")
-        for (i in 1:dd) {
-            attr_name <- colnames(d)[i+1]
-            result0 <- if (is.numeric(d[[attr_name]])) 
-                .evaluateNumericAttribute(d, attr_name, minsplit=minsplit) else         
-                    .evaluateCategoricalAttribute(d, attr_name, minsplit=minsplit)
-            cNodeChoices <- data.frame(res0=result0$X, score=result0$score, 
-                                       v=paste(result0$v, collapse=","), 
-                                       stringsAsFactors=F)
-            nodeChoices <- rbind(nodeChoices, cNodeChoices, stringsAsFactors=F)
-            if (result0$score > result$score) result <- result0
-        }
-        colnames(nodeChoices) <- c(paste0("--", result$X, "--"), "score", "v")
-        nodeChoices <- nodeChoices[order(nodeChoices$score, decreasing=T),]
-        rownames(nodeChoices) <- NULL
-        if (is.numeric(result$v)) {
-            dy <- d[d[[result$X]] <= result$v,]
-            dn <- d[d[[result$X]] > result$v,]
-        } else {
-            dy <- d[d[[result$X]] %in% result$v,]
-            dn <- d[!d[[result$X]] %in% result$v,]
-        }
-        env$node <- env$node + 1
-        env$nodeChoices[[env$node]] <- nodeChoices
-        v <- if (is.numeric(result$v)) paste0("mniejsze niż\n", result$v) else paste(result$v, collapse=",\n")
-        vC <- if (is.numeric(result$v)) paste0("większe niż\n", result$v) else paste(setdiff(cs[[result$X]], result$v) , collapse=",\n")
-        te <- data.frame(level=level,
-                         parent=parent,
-                         node=env$node, 
-                         leaf=0,
-                         LR=LR,
-                         Lsize=nrow(dy),
-                         Lleft=if(is.na(table(dy[[1]])[allX][1])) 0 else table(dy[[1]])[allX][1],
-                         Lright=if(is.na(table(dy[[1]])[allX][2])) 0 else table(dy[[1]])[allX][2],
-                         Rsize=nrow(dn),
-                         Rleft=if(is.na(table(dn[[1]])[allX][1])) 0 else table(dn[[1]])[allX][1],
-                         Rright=if(is.na(table(dn[[1]])[allX][2])) 0 else table(dn[[1]])[allX][2],
-                         v=v,
-                         vC=vC,
-                         vName=result$X,
-                         stringsAsFactors=F)
-        rownames(te) <- NULL
-        env$resultDF <- rbind(env$resultDF, te)
-        .decisionTreeRecursive(dy, eta=eta, purity=purity, LR="L", env=env, 
-                               allX=allX, minsplit=minsplit, cs=cs) 
-        .decisionTreeRecursive(dn, eta=eta, purity=purity, LR="R", env=env, 
-                               allX=allX, minsplit=minsplit, cs=cs)
+  }
+  if (nrow(d) < eta | d_purity > purity) {  # leaf
+    env$node <- env$node + 1
+    env$leaf <- env$leaf + 1
+    te <- data.frame(level = level,
+                     parent = parent,
+                     node = env$node,
+                     leaf = env$leaf,
+                     LR = LR,
+                     Lsize = nrow(d),
+                     Lleft = if(is.na(table(d[[1]])[allX][1])) 0 else table(d[[1]])[allX][1],
+                     Lright = if(is.na(table(d[[1]])[allX][2])) 0 else table(d[[1]])[allX][2],
+                     Rsize = 0,
+                     Rleft = 0,
+                     Rright = 0,
+                     v = "",
+                     vC = "",
+                     vName = "leaf",
+                     stringsAsFactors = F)
+    rownames(te) <- NULL
+    env$resultDF <- rbind(env$resultDF, te)
+    return() 
+  } else {  # node
+    nodeChoices <- data.frame(res0 = character(), score=character(), 
+                              v = character(), stringsAsFactors=F)
+    dd <- ncol(d) - 1  # number of attributes
+    result <- list(v = 0, score=0, X="")
+    for (i in 1:dd) {
+      attr_name <- colnames(d)[i+1]
+      result0 <- if (is.numeric(d[[attr_name]])) 
+        .evaluateNumericAttribute(d, attr_name, minsplit = minsplit) else         
+          .evaluateCategoricalAttribute(d, attr_name, minsplit = minsplit)
+      cNodeChoices <- data.frame(res0 = result0$X, score=result0$score, 
+                                 v = paste(result0$v, collapse=","), 
+                                 stringsAsFactors = F)
+      nodeChoices <- rbind(nodeChoices, cNodeChoices, stringsAsFactors = F)
+      if (result0$score > result$score) result <- result0
     }
+    colnames(nodeChoices) <- c(paste0("--", result$X, "--"), "score", "v")
+    nodeChoices <- nodeChoices[order(nodeChoices$score, decreasing = T),]
+    rownames(nodeChoices) <- NULL
+    if (is.numeric(result$v)) {
+      dy <- d[d[[result$X]] <= result$v,]
+      dn <- d[d[[result$X]] > result$v,]
+    } else {
+      dy <- d[d[[result$X]] %in% result$v,]
+      dn <- d[!d[[result$X]] %in% result$v,]
+    }
+    env$node <- env$node + 1
+    env$nodeChoices[[env$node]] <- nodeChoices
+    v <- if (is.numeric(result$v)) paste0("less than\n", result$v) else paste(result$v, collapse = ",\n")
+    vC <- if (is.numeric(result$v)) paste0("more than\n", result$v) else paste(setdiff(cs[[result$X]], result$v) , collapse = ",\n")
+    te <- data.frame(level = level,
+                     parent = parent,
+                     node = env$node, 
+                     leaf = 0,
+                     LR = LR,
+                     Lsize = nrow(dy),
+                     Lleft = if(is.na(table(dy[[1]])[allX][1])) 0 else table(dy[[1]])[allX][1],
+                     Lright = if(is.na(table(dy[[1]])[allX][2])) 0 else table(dy[[1]])[allX][2],
+                     Rsize = nrow(dn),
+                     Rleft = if(is.na(table(dn[[1]])[allX][1])) 0 else table(dn[[1]])[allX][1],
+                     Rright = if(is.na(table(dn[[1]])[allX][2])) 0 else table(dn[[1]])[allX][2],
+                     v = v,
+                     vC = vC,
+                     vName = result$X,
+                     stringsAsFactors = F)
+    rownames(te) <- NULL
+    env$resultDF <- rbind(env$resultDF, te)
+    .decisionTreeRecursive(dy, eta = eta, purity=purity, LR="L", env=env, 
+                           allX = allX, minsplit=minsplit, cs=cs) 
+    .decisionTreeRecursive(dn, eta = eta, purity=purity, LR="R", env=env, 
+                           allX = allX, minsplit=minsplit, cs=cs)
+  }
 }
-
-# d <- iris[, c("Species", "Sepal.Length", "Sepal.Width")]
-# d$Species <- as.character(d$Species)
-# d$Species[d$Species != "setosa"] <- "non-setosa"
-# dd <- decisionTree(d, eta=5, purity=0.95, minsplit=0)
 
--- R/plot.DecisionTreeObject.R
+++ R/plot.DecisionTreeObject.R
@@ -21,114 +21,97 @@
 #' plot(decTree)
 
 plot.DecisionTreeObject <- function(DecisionTreeObject, dict, green=1, left=1) {
-    # ---- prepare data
-    treeResult <- DecisionTreeObject@resultDF
-    nLevel <- max(treeResult$level)
-    d <- cbind(treeResult, x=0, y=0)
-    d$x[d$leaf != 0] <- d$leaf[d$leaf != 0]
-    d$x[d$leaf != 0] <- d$leaf[d$leaf != 0]
-    d$y <- d$level
+  # ---- prepare data
+  treeResult <- DecisionTreeObject@resultDF
+  nLevel <- max(treeResult$level)
+  d <- cbind(treeResult, x=0, y=0)
+  d$x[d$leaf != 0] <- d$leaf[d$leaf != 0]
+  d$x[d$leaf != 0] <- d$leaf[d$leaf != 0]
+  d$y <- d$level
 
-    # ---- set values on leaves
-    d <- cbind(d, name=as.character(d$vName), stringsAsFactors=F)
-    if (left == 1) {
-        d$name[d$leaf != 0] <- paste0(d$Lleft[d$leaf != 0], "/", 
-                                      d$Lsize[d$leaf != 0], "\n",
-                                      round(d$Lleft[d$leaf != 0] / 
-                                            d$Lsize[d$leaf != 0] * 100), "%")
-    } else {
-        d$name[d$leaf != 0] <- paste0(d$Lright[d$leaf != 0], "/", 
-                                      d$Lsize[d$leaf != 0], "\n",
-                                      round(d$Lright[d$leaf != 0] / 
-                                            d$Lsize[d$leaf != 0] * 100), "%")
+  # ---- set values on leaves
+  d <- cbind(d, name=as.character(d$vName), stringsAsFactors=F)
+  if (left == 1) {
+    d$name[d$leaf != 0] <- paste0(d$Lleft[d$leaf != 0], "/", 
+                                  d$Lsize[d$leaf != 0], "\n",
+                                  round(d$Lleft[d$leaf != 0] / 
+                                        d$Lsize[d$leaf != 0] * 100), "%")
+  } else {
+    d$name[d$leaf != 0] <- paste0(d$Lright[d$leaf != 0], "/", 
+                                  d$Lsize[d$leaf != 0], "\n",
+                                  round(d$Lright[d$leaf != 0] / 
+                                        d$Lsize[d$leaf != 0] * 100), "%")
 
-    }
+  }
 
-    for (i in (nLevel - 1):1) {
-        for (j in d$node) {
-            if (d$level[j] == i & d$leaf[j] == 0) {
-                crow <- d[d$node == j,]
-                d$x[j] <- (d$x[d$parent == j & d$LR == "L"] + 
-                           d$x[d$parent == j & d$LR == "R"]) / 2
-            }
-        }
+  for (i in (nLevel - 1):1) {
+    for (j in d$node) {
+      if (d$level[j] == i & d$leaf[j] == 0) {
+        crow <- d[d$node == j,]
+        d$x[j] <- (d$x[d$parent == j & d$LR == "L"] + 
+                   d$x[d$parent == j & d$LR == "R"]) / 2
+      }
     }
+  }
 
-    # ---- plot edges
-    plot.new()
-    plot.window(c(0.5, nrow(d[d$leaf != 0,]) + 0.5), c(nLevel, 1))
+  # ---- plot edges
+  plot.new()
+  plot.window(c(0.5, nrow(d[d$leaf != 0,]) + 0.5), c(nLevel, 1))
 
-    d <- cbind(d, parx=0, pary=0)
-    #     y <- seq(0, pi, 0.001)
-    #     x <- -cos(y)
-    #     y <- y/pi * 0.8
-    #     x <- x/2 + 0.5 # poprawka na szerokość smugi, żeby w child trafiała na środek
-    for (i in 2:nrow(d)) {
-        cpar <- d[d$node == d[i, "parent"],]  # parent
-        width <- (d$Lsize[i] + d$Rsize[i]) / (d[1,"Lsize"] + d[1, "Rsize"]) * 80
-        y <- seq(0, pi, length.out=30)
-        x <- cos(y)
-        lines((x / 2 + 1/2) * (cpar$x - d[i, "x"]) + d[i, "x"],
-              y / pi + cpar$level, lwd = width, col="gray") 
-        #     me <- d[d$node == i,]  # me
-        #     cpar <- d[d$node == d[i, "parent"],]  # parent
-        #     bro <- d[d$parent == cpar$node & d$LR != me$LR,]  # brother
-        #     width <- me$Lsize + me$Rsize 
-        #     fullWidth <- bro$Lsize + bro$Rsize 
-        #     
-        #     bwidth <-   # brother's width
-        #     rect(x * (d[i, "x"] - cpar$x) + cpar$x - fullWidth / 2, 
-        #          y + cpar$y + 0.1, 
-        #          x * (d[i, "x"] - cpar$x) + cpar$x + width, 
-        #          y + cpar$y + 0.1 + 0.001, 
-        #          col="gray", border=NA)
-        #     lines(x+cpar$x, y+cpar$y+0.1, col="gray")
-        #     lines(x+cpar$x+0.3, y+cpar$y+0.1, col="gray")
-    }
-    rect(0.5, 0, nrow(d[d$leaf != 0,]) + 0.5, 1, col="white", border=NA)
-    # ---- plot vertices
-    color <- rep("white", nrow(d))
-    cols <- (d$Lleft + d$Rleft) / (d$Lsize + d$Rsize) * 255 
-    if (green == 1) {
-        color <- paste0("#", as.hexmode(round(255 - cols)), 
-                        as.hexmode(round(cols)), "00")
-    } else {
-        color <- paste0("#", as.hexmode(round(cols)), 
-                        as.hexmode(round(255 - cols)), "00")
-    }
-    widthCoef <- rep(1.8, nrow(d))
-    widthCoef[d$leaf != 0] <- 1.1
-    if (missing(dict)) {
-        widths <- strwidth(d$name)/widthCoef
-        widths[widths < 0.45 & d$leaf == 0] <- 0.45
-        rect(d$x - widths, d$y - 0.15 * (nLevel - 1) / 3,  
-             d$x + widths, d$y + 0.15 * (nLevel - 1) / 3, 
-             col=color, border="gray")
-        text(d$x, d$y, d$name)
-    } else {
-        dd <- merge(d, dict, by="vName", all=T)
-        dd$toPlot[dd$vName == "leaf"] <- dd$name[dd$vName == "leaf"]
-        dd <- dd[order(dd$node),]
-        widths <- strwidth(dd$toPlot)/widthCoef
-        widths[widths < 0.45 & d$leaf == 0] <- 0.45
-        rect(dd$x - widths, dd$y - 0.15 * (nLevel - 1) / 3,
-             dd$x + widths, dd$y + 0.15 * (nLevel - 1) / 3, 
-             col=color, border="gray")
-        text(dd$x, dd$y, dd$toPlot)
-    }
+  d <- cbind(d, parx=0, pary=0)
+  for (i in 2:nrow(d)) {
+    cpar <- d[d$node == d[i, "parent"],]  # parent
+    width <- (d$Lsize[i] + d$Rsize[i]) / (d[1,"Lsize"] + d[1, "Rsize"]) * 80
+    y <- seq(0, pi, length.out=30)
+    x <- cos(y)
+    lines((x / 2 + 1/2) * (cpar$x - d[i, "x"]) + d[i, "x"],
+          y / pi + cpar$level, lwd = width, col="gray") 
+  }
+  rect(0.5, 0, nrow(d[d$leaf != 0,]) + 0.5, 1, col="white", border=NA)
 
-    # ---- plot conditions on edges
-    for (i in 1:nrow(d)) {
-        if (d$v[i] != "") {
-            Lchild <- d[d$parent == i & d$LR == "L",]
-            Rchild <- d[d$parent == i & d$LR == "R",]
-            x <- d$x[i] + (Lchild$x - d$x[i] ) / 2 - 0.1  # TODO dostosowac do liczby liści
-            y <- d$y[i] + (Lchild$y - d$y[i] ) / 2 
-            text(x, y, d$v[i]) 
-            x <- d$x[i] + (Rchild$x - d$x[i] ) / 2 + 0.1
-            y <- d$y[i] + (Rchild$y - d$y[i] ) / 2 
-            text(x, y, d$vC[i])
-        }
+  # ---- plot vertices
+  color <- rep("white", nrow(d))
+  cols <- (d$Lleft + d$Rleft) / (d$Lsize + d$Rsize) * 255 
+  if (green == 1) {
+    color <- paste0("#", as.hexmode(round(255 - cols)), 
+                    as.hexmode(round(cols)), "00")
+  } else {
+    color <- paste0("#", as.hexmode(round(cols)), 
+                    as.hexmode(round(255 - cols)), "00")
+  }
+  widthCoef <- rep(1.8, nrow(d))
+  widthCoef[d$leaf != 0] <- 1.1
+  if (missing(dict)) {
+    widths <- strwidth(d$name)/widthCoef
+    widths[widths < 0.45 & d$leaf == 0] <- 0.45
+    rect(d$x - widths, d$y - 0.15 * (nLevel - 1) / 3,  
+         d$x + widths, d$y + 0.15 * (nLevel - 1) / 3, 
+         col=color, border="gray")
+    text(d$x, d$y, d$name)
+  } else {
+    dd <- merge(d, dict, by="vName", all=T)
+    dd$toPlot[dd$vName == "leaf"] <- dd$name[dd$vName == "leaf"]
+    dd <- dd[order(dd$node),]
+    widths <- strwidth(dd$toPlot)/widthCoef
+    widths[widths < 0.45 & d$leaf == 0] <- 0.45
+    rect(dd$x - widths, dd$y - 0.15 * (nLevel - 1) / 3,
+         dd$x + widths, dd$y + 0.15 * (nLevel - 1) / 3, 
+         col=color, border="gray")
+    text(dd$x, dd$y, dd$toPlot)
+  }
+
+  # ---- plot conditions on edges
+  for (i in 1:nrow(d)) {
+    if (d$v[i] != "") {
+      Lchild <- d[d$parent == i & d$LR == "L",]
+      Rchild <- d[d$parent == i & d$LR == "R",]
+      x <- d$x[i] + (Lchild$x - d$x[i] ) / 2 - 0.1  # TODO number of leaves
+      y <- d$y[i] + (Lchild$y - d$y[i] ) / 2 
+      text(x, y, d$v[i]) 
+      x <- d$x[i] + (Rchild$x - d$x[i] ) / 2 + 0.1
+      y <- d$y[i] + (Rchild$y - d$y[i] ) / 2 
+      text(x, y, d$vC[i])
     }
+  }
 }
 
